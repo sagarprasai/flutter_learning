ceb3921743a4529927571c7c2916fc8671b739fb
Sagar_Prasai <sagar.prasain@gmail.com>
2023-06-20 04:35:14 +0000

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,69 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'data/collections.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return (MaterialApp(
+        title: "Flutter Demo",
+        color: Colors.blue,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: MyHomePage()));
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+// class MyHomePage extends StatelessWidget {
+//   late String? title;
+//   MyHomePage({super.key, required title});
+
+//   @override
+//   Widget build(BuildContext build) {
+//     return (Scaffold(
+//       appBar:
+//           AppBar(backgroundColor: Colors.orange, title: const Text("Homepage")),
+//       body: Center(child: Text("Hello World!")),
+//     ));
+//   }
+// }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+  String? startupName = 'Your startup Name';
+
+  // from 0 upto 99 included
+
+  void generator() {
+    Random random = new Random();
+    int randomNumber1 = random.nextInt(49);
+    int randomNumber2 = random.nextInt(49);
+    setState(() {
+      startupName = collection1[randomNumber1] + collection2[randomNumber2];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
+    return (Scaffold(
+      appBar: AppBar(title: Text('Startup Name Generate')),
       body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+          child: Text(
+        '$startupName',
+        textScaleFactor: 2.0,
+      )),
+      floatingActionButton:
+          FloatingActionButton(onPressed: generator, child: (Icon(Icons.add))),
+    ));
   }
 }
